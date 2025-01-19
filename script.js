568ebf02cdf2540d0b5efc3eea51348210ee1800
Daniel Felipe Gómez Ferreira <pipeferreira1@gmail.com>
2025-01-19 14:24:11 -0500

    Primera version del proyecto}

---
+++ script.js
@@ -0,0 +1,20 @@
+const aboutText="La Fundación Deportiva Elina Morales tiene como objetivo incentivar la práctica deportiva con el fin de llegar a aquellos niños y jóvenes que se encuentren en situaciones desfavorecidas, sin recursos o con alguna discapacidad física o psíquica y con riesgo de exclusión social, apoyándolos en lo social.";
+document.getElementById("about-text").textContent=aboutText;
+
+const aboutMision ="mejorar la calidad de vida de los deportistas en diferentes categorías y edades de bajos recursos, con habilidades deportivas a través de la promoción de oportunidades de formación, desarrollo, rehabilitación física y psicológica deportiva, contando con el apoyo de los patrocinadores voluntarios y el capital humano altamente capacitado que lo integra.";
+document.getElementById("about-mision").textContent=aboutMision;
+
+const aboutVision =" ser una fundación reconocida por la comunidad, instituciones públicas y privadas, como también de los atletas de alto rendimiento y proyectar las capacidades de sus beneficiarios desde la articulación y apoyo en las diferentes áreas deportivas, asimismo el compromiso de sus patrocinadores, colaboradores y de ser una entidad económicamente autosostenible.";
+document.getElementById("about-vision").textContent=aboutVision;
+
+
+const descripcion =" Conoce nuestras iniciativas: desde programas educativos hasta proyectos de sostenibilidad ambiental.";
+document.getElementById("descripcion").textContent=descripcion;
+
+
+//------------------------------------EVENTOS---------------------------------------------
+/////primer evento
+const sombLoco = "Sombrero loco";
+document.getElementById("tituloSombLoco").textContent=sombLoco;
+const descripcionSombLoco = "El 17 de diciembre se celebró la competencia del Sombrero Loco en el Parque Mareigua Paramo, reuniendo a participantes de todas las edades para mostrar sombreros originales y creativos. Los concursantes presentaron diseños extravagantes, que fueron evaluados por su creatividad, originalidad y técnica. Todos recibieron premios por su esfuerzo. Además, hubo actividades para toda la familia, como bailoterapia, música en vivo y juegos. Los organizadores agradecieron la participación y anunciaron planes para futuras ediciones, manteniendo viva la tradición de creatividad y diversión.";
+document.getElementById("descripcionSombLoco").textContent=descripcionSombLoco;
